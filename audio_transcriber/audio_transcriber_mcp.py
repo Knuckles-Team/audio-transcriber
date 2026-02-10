19d9cb4a6c31494bfdc1b3b090b4411e571ebe9d
KnucklesSG1 <knucklessg1@gmail.com>
2026-02-10 02:03:25 -0600

    Bump version: 0.6.0 → 0.6.1

--- audio_transcriber/audio_transcriber_mcp.py
+++ audio_transcriber/audio_transcriber_mcp.py
@@ -29,7 +29,7 @@ from audio_transcriber.middlewares import (
     JWTClaimsLoggingMiddleware,
 )
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 logger = get_logger(name="TokenMiddleware")
 logger.setLevel(logging.DEBUG)
