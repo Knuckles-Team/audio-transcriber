1b273d4671fa9bcfad21f7d9ce7cd6dbe5acb375
KnucklesSG1 <knucklessg1@gmail.com>
2026-02-27 02:46:46 -0600

    Bump version: 0.6.20 → 0.6.21

--- audio_transcriber/audio_transcriber.py
+++ audio_transcriber/audio_transcriber.py
@@ -15,7 +15,7 @@ import pyaudio
 import wave
 import asyncio
 
-__version__ = "0.6.20"
+__version__ = "0.6.21"
 
 
 class TranscriberBackend(ABC):
--- audio_transcriber/mcp.py
+++ audio_transcriber/mcp.py
@@ -31,7 +31,7 @@ from agent_utilities.middlewares import (
     JWTClaimsLoggingMiddleware,
 )
 
-__version__ = "0.6.20"
+__version__ = "0.6.21"
 
 logger = get_logger(name="TokenMiddleware")
 logger.setLevel(logging.DEBUG)
